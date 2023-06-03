4131640d257ab4e6689f16e458b0cd75abea1b19
ashtron <ashton.playsted@protonmail.com>
2023-06-02 22:22:47 -0700

    Can insert messages and subscribe to new messages

--- app/page.js
+++ app/page.js
@@ -3,23 +3,68 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./home.css";
 
 export default function Home() {
-  const [messages, setMessages] = useState([
-    "hello",
-    "what's up",
-    "nothing much hbu",
-    "same",
-    "bye then",
-    "bye",
-  ]);
-
   const supabase = createPagesBrowserClient();
 
-  const handleClick = () => {};
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messages = await supabase.from("messages").select("*");
+
+      setMessages(messages.data.map((item) => item.content));
+    };
+
+    fetchMessages();
+  }, []);
+
+  const [messageInput, setMessageInput] = useState("");
+
+  // const messagesSubscripchantion = supabase
+  //   .from("messages")
+  //   .select("*")
+  //   .on("INSERT", (data) => {
+  //     setMessages([...messages, data.content]);
+  //   })
+  //   .subscribe();
+
+  const channel = supabase
+    .channel("table_db_changes")
+    .on(
+      "postgres_changes",
+      {
+        event: "INSERT",
+        schema: "public",
+        table: "messages",
+      },
+      (payload) => {
+        setMessages([...messages, payload.new.content]);
+      }
+    )
+    .subscribe();
+
+  const insertMessage = async (message) => {
+    await supabase
+      .from("messages")
+      .insert([{ content: message }])
+      .select();
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    insertMessage(messageInput);
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setMessageInput(event.target.value);
+  };
 
   return (
     <article>
@@ -30,6 +75,7 @@ export default function Home() {
         })}
       </ul>
       <footer>
+        <input value={messageInput} onChange={handleChange} />
         <button onClick={handleClick}>Send</button>
       </footer>
     </article>
